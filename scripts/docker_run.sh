7fc51ae5d761cd10f006275a21da12925c622531
jdiamond <feros32@gmail.com>
2026-05-15 22:48:38 -0700

    Fix path.

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -5,6 +5,8 @@ ENV_PATH=$1
 SCRIPT_DIR="$(dirname "$0")"
 CACHE_DIR="$SCRIPT_DIR/../.cache"
 
+cd "$SCRIPT_DIR/.."
+
 docker build -t pcb_map .
 docker rm pcb_map_locations
 docker run -d --restart=always --env-file $ENV_PATH -v "$CACHE_DIR:/app/.cache" --name pcb_map_locations pcb_map
